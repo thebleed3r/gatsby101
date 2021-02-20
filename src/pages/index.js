b6e5cb2062c93f6b29146c32da57054296e5869e
theBleed3r <thebleed3r@gmail.com>
2021-02-20 05:29:04 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
+
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby"/>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
+}
